78f1413ae8e515f09461ae73851038bb7c92ad18
Rian Carlo <riansantos.dev@gmail.com>
2023-07-20 18:18:27 -0300

    Update PlocadoraApplication.java

--- src/main/java/com/plocadora/projeto/PlocadoraApplication.java
+++ src/main/java/com/plocadora/projeto/PlocadoraApplication.java
@@ -2,8 +2,8 @@ package com.plocadora.projeto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PlocadoraApplication {
 
 	public static void main(String[] args) {
